d24e5e42380171967741e147c9f8799d84b0cded
rickymangla <100446433+rickymangla@users.noreply.github.com>
2024-10-17 21:03:28 -0400

    line added in Varun.java
    line added in Varun.java

--- src/Varun.java
+++ src/Varun.java
@@ -2,5 +2,6 @@ public class Varun {
     public static void main(String[] args) {
         System.out.println("Varun");
         System.out.println("robin");
+        System.out.println("Amit");
     }
 }
